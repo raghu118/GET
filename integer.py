0120475026c89c7a7b838cd4c8ac3c2c439f3f74
raghu118 <56404269+raghu118@users.noreply.github.com>
2020-02-20 11:46:03 +0530

    Integer python script

---
+++ integer.py
@@ -0,0 +1,23 @@
+a = int(input("Input an integer : "))
+          
+P = ( a % 3)
+if P==0:
+  print"P"
+
+a = int(input("Input an integer : "))
+E = ( a % 5)
+if E==0:
+  print"E"
+
+a = int(input("Input an integer : "))
+b = ( a % 5)
+c = ( a % 3)
+
+if b==0 & c==0:
+    print "PE"
+   
+
+    
+    
+    
+
